28de75d32a24025d84b2ff0d739167fde01b0e4d
Zoey-xue47 <450287421@qq.com>
2022-05-07 11:03:22 +0800

    add

---
+++ tongxunluguanli/build/CMakeFiles/cpp_test.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/zoey/tmp/exercise/basic.cpp" "/home/zoey/tmp/build/CMakeFiles/cpp_test.dir/exercise/basic.cpp.o"
+  "/home/zoey/tmp/exercise/clamb.cpp" "/home/zoey/tmp/build/CMakeFiles/cpp_test.dir/exercise/clamb.cpp.o"
+  "/home/zoey/tmp/exercise/consider.cpp" "/home/zoey/tmp/build/CMakeFiles/cpp_test.dir/exercise/consider.cpp.o"
+  "/home/zoey/tmp/exercise/core.cpp" "/home/zoey/tmp/build/CMakeFiles/cpp_test.dir/exercise/core.cpp.o"
+  "/home/zoey/tmp/exercise/middle.cpp" "/home/zoey/tmp/build/CMakeFiles/cpp_test.dir/exercise/middle.cpp.o"
+  "/home/zoey/tmp/exercise/swap.cpp" "/home/zoey/tmp/build/CMakeFiles/cpp_test.dir/exercise/swap.cpp.o"
+  "/home/zoey/tmp/main.cpp" "/home/zoey/tmp/build/CMakeFiles/cpp_test.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ tongxunluguanli/build/CMakeFiles/cpp_test.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_test.dir/exercise/basic.cpp.o"
+  "CMakeFiles/cpp_test.dir/exercise/clamb.cpp.o"
+  "CMakeFiles/cpp_test.dir/exercise/consider.cpp.o"
+  "CMakeFiles/cpp_test.dir/exercise/core.cpp.o"
+  "CMakeFiles/cpp_test.dir/exercise/middle.cpp.o"
+  "CMakeFiles/cpp_test.dir/exercise/swap.cpp.o"
+  "CMakeFiles/cpp_test.dir/main.cpp.o"
+  "cpp_test"
+  "cpp_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
